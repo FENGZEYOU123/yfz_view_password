d01759f94bb1c5dfdc19b5cb67a13f30f0ad2ff3
FENGZEYOU123 <35758413+FENGZEYOU123@users.noreply.github.com>
2021-04-14 17:06:35 +0800

    new project

---
+++ app/src/main/java/com/yfz/password/MainActivity.java
@@ -0,0 +1,15 @@
+package com.yfz.password;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
+}
